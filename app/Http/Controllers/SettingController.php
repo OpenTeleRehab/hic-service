f1cddb99c77fd33e5d7339c378dd22f70f7e7fb7
Chanthorn Ngeth <chanthorn@web-essentials.asia>
2020-11-20 14:59:01 +0700

    [IMP] Implement api for language, limit_patient
    refs TAR-19

---
+++ app/Http/Controllers/SettingController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Resources\ProfessionResource;
+use App\Models\Profession;
+use Illuminate\Http\Request;
+
+class SettingController extends Controller
+{
+    /**
+     * @param \Illuminate\Http\Request $request
+     *
+     * @return array
+     */
+    public function getLanguage(Request $request)
+    {
+        $languages = [
+            ['id' => 1, 'name' => 'English'],
+            ['id' => 2, 'name' => 'Khmer'],
+            ['id' => 3, 'name' => 'Vietnamese']
+        ];
+        return ['success' => true, 'data' => $languages];
+    }
+    /**
+     * @param \Illuminate\Http\Request $request
+     *
+     * @return array
+     */
+    public function getDefaultLimitedPatient(Request $request)
+    {
+        $defaultLimitedPatient = ['value' => 15];
+
+        return ['success' => true, 'data' => $defaultLimitedPatient];
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\ClinicController;
 use App\Http\Controllers\CountryController;
 use App\Http\Controllers\ProfessionController;
+use App\Http\Controllers\SettingController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AdminController;
 
@@ -21,3 +22,5 @@ Route::apiResource('admin', AdminController::class);
 Route::apiResource('country', CountryController::class);
 Route::apiResource('clinic', ClinicController::class);
 Route::apiResource('profession', ProfessionController::class);
+Route::get('getLanguage', [SettingController::class, 'getLanguage']);
+Route::get('getDefaultLimitedPatient', [SettingController::class, 'getDefaultLimitedPatient']);
