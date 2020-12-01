536616812100aa3f4bebd4df68d00f8c63cda78c
Chanthorn Ngeth <chanthorn@web-essentials.asia>
2020-12-01 16:19:49 +0700

    [TASK] Adjust api of getting country, clinic
    refs TRA-42

--- app/Http/Resources/ClinicResource.php
+++ app/Http/Resources/ClinicResource.php
@@ -15,6 +15,7 @@ class ClinicResource extends JsonResource
     public function toArray($request)
     {
         return [
+            'id' => $this->id,
             'identity' => $this->identity,
             'name' => $this->name
         ];
