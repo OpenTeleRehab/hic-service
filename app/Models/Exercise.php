c8add14cb2adb30a31e09689d28a8b295fa68ddb
Chivy Lim <chivy@web-essentials.asia>
2020-12-04 09:57:05 +0700

    [IMP] Set default order for exercise model
    refs TRA-25

--- app/Models/Exercise.php
+++ app/Models/Exercise.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -23,5 +24,20 @@ class Exercise extends Model
     {
         // Todo: check if it is not used.
         return $this->include_feedback;
+    }
+
+    /**
+     * Bootstrap the model and its traits.
+     *
+     * @return void
+     */
+    protected static function boot()
+    {
+        parent::boot();
+
+        // Set default order by title.
+        static::addGlobalScope('order', function (Builder $builder) {
+            $builder->orderBy('title');
+        });
     }
 }
