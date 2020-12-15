b34d9a0f2c191eea016e96d01de763cac5442f89
Chanthorn Ngeth <chanthorn@web-essentials.asia>
2020-12-15 15:07:28 +0700

    [TASK] Implement api to store country
    refs TRA-102

--- app/Http/Controllers/CountryController.php
+++ app/Http/Controllers/CountryController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Http\Resources\CountryResource;
 use App\Models\Country;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class CountryController extends Controller
 {
@@ -18,5 +19,28 @@ class CountryController extends Controller
         $countries = Country::all();
 
         return ['success' => true, 'data' => CountryResource::collection($countries)];
+    }
+
+    /**
+     * @param \Illuminate\Http\Request $request
+     *
+     * @return array|void
+     */
+    public function store(Request $request)
+    {
+        $isoCode = $request->get('iso_code');
+        $availableCountry = Country::where('iso_code', $isoCode)->count();
+        if ($availableCountry) {
+            return abort(409, 'error_message.country_exists');
+        }
+
+        Country::create([
+            'iso_code' => $isoCode,
+            'name' => $request->get('name'),
+            'phone_code' => $request->get('phone_code'),
+            'language_id' => $request->get('language')
+        ]);
+
+        return ['success' => true, 'message' => 'success_message.country_add'];
     }
 }
