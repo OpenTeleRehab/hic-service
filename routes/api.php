99e0787b5273ddcb072f1b504e8ece28d221d288
Chivy Lim <chivy@web-essentials.asia>
2020-12-28 08:32:55 +0700

    [TASK] Request translation of language param
    refs TRA-185

--- routes/api.php
+++ routes/api.php
@@ -23,22 +23,25 @@ use \App\Http\Controllers\FileController;
 |
 */
 
-Route::apiResource('admin', AdminController::class);
+Route::group(['middleware' => 'auth:api'], function () {
+    Route::apiResource('admin', AdminController::class);
+    Route::apiResource('translation', TranslationController::class);
+
+    Route::get('user/profile', [ProfileController::class, 'getUserProfile']);
+    Route::put('user/update-password', [ProfileController::class, 'updatePassword']);
+    Route::put('user/update-information', [ProfileController::class, 'updateUserProfile']);
+});
+
+// Todo: apply for Admin, Therapist, Patient APPs
 Route::apiResource('country', CountryController::class);
 Route::apiResource('clinic', ClinicController::class);
+Route::apiResource('language', LanguageController::class);
+Route::apiResource('file', FileController::class);
+Route::get('getDefaultLimitedPatient', [SettingController::class, 'getDefaultLimitedPatient']);
 Route::apiResource('profession', ProfessionController::class);
 Route::apiResource('exercise', ExerciseController::class);
 Route::get('exercise/list/by-ids', [ExerciseController::class, 'getByIds']);
 
-Route::apiResource('file', FileController::class);
+
+// Public access
 Route::get('translation/i18n/{platform}', [TranslationController::class, 'getI18n']);
-
-Route::get('getDefaultLimitedPatient', [SettingController::class, 'getDefaultLimitedPatient']);
-
-Route::group(['middleware' => 'auth:api'], function () {
-    Route::get('user/profile', [ProfileController::class, 'getUserProfile']);
-    Route::put('user/update-password', [ProfileController::class, 'updatePassword']);
-    Route::put('user/update-information', [ProfileController::class, 'updateUserProfile']);
-    Route::apiResource('translation', TranslationController::class);
-    Route::apiResource('language', LanguageController::class);
-});
