dc23aaf6b834698498a7b32554efe618b7b58739
Lysa Thorn <lysa@web-essentials.co>
2021-02-04 16:32:03 +0700

    [TASK] Test feature for language
    refs TRA-303

---
+++ tests/Feature/LanguageTest.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+use App\Models\Language;
+
+class LanguageTest extends TestCase
+{
+    /**
+     * @return void
+     */
+    public function setUp(): void
+    {
+        parent::setUp();
+        $this->initDefaultData();
+    }
+
+    /**
+     * @group FeatureListLanguageTest
+     *
+     * @return void
+     */
+    public function testListLanguage()
+    {
+        $globalAdmin = $this->getGlobalAdmin();
+        $response = $this->actingAs($globalAdmin)->get('/api/language');
+        $response->assertStatus(200);
+    }
+
+    /**
+     * @group FeatureCreateLanguageTest
+     *
+     * @return void
+     */
+    public function testCreateLanguage()
+    {
+        $globalAdmin = $this->getGlobalAdmin();
+        $response = $this->actingAs($globalAdmin)->post('/api/language',[
+            'name' => 'Cambodai',
+            'code' => 'kh'
+        ]);
+        $response->assertJson(['success' => true,"message" => "success_message.language_add"]);
+        $response->assertStatus(200);
+        $this->assertDatabaseCount('languages', 1);
+    }
+
+    /**
+     * @group FeatureEditLanguageTest
+     *
+     * @return void
+     */
+    public function testEditLanguage()
+    {
+        $language = factory(Language::class)->create();
+        $globalAdmin = $this->getGlobalAdmin();
+        $response = $this->actingAs($globalAdmin)
+            ->put('/api/language/'.$language->id, [
+                'name' => 'Thailand',
+                'code' => $language->code
+            ]);
+
+        $response->assertJson(['success' => true,"message" => "success_message.language_update"]);
+        $response->assertStatus(200);
+        $this->assertDatabaseHas('languages', [
+            'name' => 'Thailand'
+        ]);
+    }
+}
--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,9 +2,39 @@
 
 namespace Tests;
 
+use App\Models\User;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use Tests\Mock\MockData;
 
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+
+    /**
+     * @return void
+     */
+    public function setUp(): void
+    {
+        parent::setUp();
+    }
+
+    /**
+     * @return void
+     */
+    protected function initDefaultData(): void
+    {
+        $this->artisan('migrate:fresh');
+        $this->artisan('db:seed', ['--env' => 'testing']);
+    }
+
+    /**
+     * @param string|null $email
+     *
+     * @return User
+     */
+    public function getGlobalAdmin(string $email = null): User
+    {
+        $email = empty($email) ? MockData::$users['global_admin'] : $email;
+        return User::where('email', '=', $email)->first();
+    }
 }
