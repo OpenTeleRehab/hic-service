74de5a90eb9c7a989d351f38d1e4153182a8480d
Lina <lina@web-essentials.co>
2021-08-12 14:36:24 +0700

    [IMP] Refactor query filter
    refs RRT-69

--- app/Exports/ExercisesExport.php
+++ app/Exports/ExercisesExport.php
@@ -4,6 +4,7 @@ namespace App\Exports;
 
 use App\Exercise;
 use App\Helpers\ExerciseHelper;
+use App\Models\Exercise;
 use Illuminate\Contracts\View\View;
 use Illuminate\Http\Request;
 use Maatwebsite\Excel\Concerns\FromView;
@@ -32,7 +33,7 @@ class ExercisesExport implements FromView
      */
     public function view(): View
     {
-        $query = ExerciseHelper::generateFilterQuery($this->request);
+        $query = ExerciseHelper::generateFilterQuery($this->request, with(new Exercise));
 
         return view('exports.exercises', [
             'exercises' => $query->get(),
