45a5733d3f0e01e57507df0d5755060a60ec0a4b
Sophea Phy <sophea@web-essentials.co>
2021-09-02 09:12:18 +0700

    [TASK] Total new translations
    refs RRT-41

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -15,18 +15,17 @@ class DashboardController extends Controller
      */
     public function getStatistics()
     {
-        // TODO: Count translation submission
-        $totalExercise = Exercise::where('status', Exercise::STATUS_APPROVED)->count();
-        $exerciseSubmission = Auth::check() ? Exercise::where('status', Exercise::STATUS_PENDING)->count() : 0;
-        $exerciseTranslation = Auth::check() ? 0 : 0;
+        $totalExercise = Exercise::where('status', Exercise::STATUS_APPROVED)->whereNull('edit_translation')->count();
+        $exerciseSubmission = Auth::check() ? Exercise::where('status', Exercise::STATUS_PENDING)->whereNull('edit_translation')->count() : 0;
+        $exerciseTranslation = Auth::check() ? Exercise::where('status', Exercise::STATUS_DRAFT)->whereNotNull('edit_translation')->count() : 0;
 
-        $totalEducationMaterial = EducationMaterial::where('status', Exercise::STATUS_APPROVED)->count();
-        $educationSubmission = Auth::check() ? EducationMaterial::where('status', Exercise::STATUS_PENDING)->count() : 0;
-        $educationTranslation = Auth::check() ? 0 : 0;
+        $totalEducationMaterial = EducationMaterial::where('status', Exercise::STATUS_APPROVED)->whereNull('edit_translation')->count();
+        $educationSubmission = Auth::check() ? EducationMaterial::where('status', Exercise::STATUS_PENDING)->whereNull('edit_translation')->count() : 0;
+        $educationTranslation = Auth::check() ? EducationMaterial::where('status', EducationMaterial::STATUS_DRAFT)->whereNotNull('edit_translation')->count() : 0;
 
-        $totalQuestionnaire = Questionnaire::where('status', Exercise::STATUS_APPROVED)->count();
-        $questionnaireSubmission = Auth::check() ? Questionnaire::where('status', Exercise::STATUS_PENDING)->count() : 0;
-        $questionnaireTranslation = Auth::check() ? 0 : 0;
+        $totalQuestionnaire = Questionnaire::where('status', Exercise::STATUS_APPROVED)->whereNull('edit_translation')->count();
+        $questionnaireSubmission = Auth::check() ? Questionnaire::where('status', Exercise::STATUS_PENDING)->whereNull('edit_translation')->count() : 0;
+        $questionnaireTranslation = Auth::check() ? Questionnaire::where('status', Questionnaire::STATUS_DRAFT)->whereNotNull('edit_translation')->count() : 0;
 
         $data = [
             'exercise' => [
