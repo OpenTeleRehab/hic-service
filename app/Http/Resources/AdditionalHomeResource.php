c82917bccb2b9323234332ec4e5d2f176fa7d30b
Chanthorn Ngeth <chanthorn@web-essentials.asia>
2021-09-06 15:04:28 +0700

    [TASK] Render featured resources
    refs RRT-15

--- app/Http/Resources/AdditionalHomeResource.php
+++ app/Http/Resources/AdditionalHomeResource.php
@@ -2,6 +2,10 @@
 
 namespace App\Http\Resources;
 
+use App\Models\EducationMaterial;
+use App\Models\Exercise;
+use App\Models\Questionnaire;
+use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
 class AdditionalHomeResource extends JsonResource
@@ -17,7 +21,41 @@ class AdditionalHomeResource extends JsonResource
         return [
             'display_quick_stat' =>  $this->display_quick_stat,
             'display_feature_resource' =>  $this->display_feature_resource,
-            'resources' => json_decode($this->resource)
+            'resources' => json_decode($this->resource),
+            'featured_resources' => $this->getFeaturedResources(json_decode($this->resource['resources']))
         ];
+    }
+
+    protected function getFeaturedResources($resources)
+    {
+        $data = [];
+        foreach ($resources as $resource) {
+            $featuredResources = [];
+            foreach ($resource as $res) {
+                $resArr = (array)$res;
+                $obj = explode('-', $resArr['id']);
+                $model = $obj[0];
+                $id = $obj[1];
+
+                $featuredResources[$model][] = $id;
+            }
+
+            if (array_key_exists('exercises', $featuredResources)) {
+                $exercises = Exercise::whereIn('id', $featuredResources['exercises'])->get();
+                $data['exercises'] = ExerciseResource::collection($exercises);
+            }
+
+            if (array_key_exists('questionnaires', $featuredResources)) {
+                $questionnaires = Questionnaire::whereIn('id', $featuredResources['questionnaires'])->get();
+                $data['questionnaires'] = QuestionnaireResource::collection($questionnaires);
+            }
+
+            if (array_key_exists('education_materials', $featuredResources)) {
+                $education_materials = EducationMaterial::whereIn('id', $featuredResources['education_materials'])->get();
+                $data['education_materials'] = EducationMaterialResource::collection($education_materials);
+            }
+        }
+
+        return $data;
     }
 }
